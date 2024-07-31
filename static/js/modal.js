7e64cc83cb9891d19939b95500ad4a8ef99fdbc0
Luke Henderson <lukehenderson@Lukes-MacBook-Pro.local>
2024-07-30 23:35:02 -0500

    finished

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,30 +1,33 @@
-// Get the modal elements
-var modal = document.getElementById("image-modal");
-var modalImg = document.getElementById("enlarged-image");
-var captionText = document.getElementById("caption");
+// Ensure the DOM is fully loaded before running the script
+document.addEventListener("DOMContentLoaded", () => {
+    // Get the modal elements
+    const modal = document.getElementById("image-modal");
+    const modalImg = document.getElementById("enlarged-image");
+    const captionText = document.getElementById("caption");
 
-// Open modal when an image is clicked
-function openModal(event) {
-    modal.style.display = "block";
-    modalImg.src = event.target.src; // Set the src of the modal image to the clicked image's src
-    captionText.innerHTML = event.target.alt; // Set the caption to the alt text
-}
+    // Open modal when an image is clicked
+    function openModal(event) {
+        modal.style.display = "block";
+        modalImg.src = event.target.src; // Set the src of the modal image to the clicked image's src
+        captionText.innerHTML = event.target.alt; // Set the caption to the alt text
+    }
 
-// Close the modal when clicking on the close button
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// Close the modal when clicking outside the image
-modal.onclick = function(event) {
-    if (event.target === modal) {
+    // Close the modal when clicking on the close button
+    const span = document.getElementsByClassName("close")[0];
+    span.onclick = function() {
         modal.style.display = "none";
     }
-}
 
-// Add event listeners to all clickable images
-var images = document.getElementsByClassName("clickable-image");
-for (var i = 0; i < images.length; i++) {
-    images[i].onclick = openModal;
-}
+    // Close the modal when clicking outside the image
+    modal.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    // Add event listeners to all clickable images
+    const images = document.getElementsByClassName("clickable-image");
+    for (let i = 0; i < images.length; i++) {
+        images[i].onclick = openModal;
+    }
+});
